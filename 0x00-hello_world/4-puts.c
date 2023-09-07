711d75dfd0536ea37e8bfd93f7a0c2171620769e
Mark-crypto <mark.onyango@strathmore.edu>
2023-09-07 03:11:27 -0700

    Betty compliant

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
